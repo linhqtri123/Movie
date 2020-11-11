47d6f392c29670c744e4b76a25dc60b667b43c33
Linh Le H. M <linh.le@asiantech.vn>
2020-11-11 14:07:44 +0700

    first commit

---
+++ app/src/main/java/com/example/movieapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.movieapp.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.movieapp.MainActivity
+import com.example.movieapp.R
+
+/**
+ * Create by Linh Le H. M. on 11/11/20
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        handleChangeActivityDelay()
+    }
+
+    private fun handleChangeActivityDelay() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
